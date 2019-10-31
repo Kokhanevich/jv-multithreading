63739caf3353c90428cb7e433adfc67063781bf3
Kokhanevich <nike.k1995@gmail.com>
2019-10-31 12:21:53 +0200

    calculated sum using ExecutorService and ForkJoin (Fixed)

--- src/main/java/ExecutorServiceMainTest.java
+++ src/main/java/ExecutorServiceMainTest.java
@@ -7,25 +7,34 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ExecutorServiceMainTest {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    private static final int POOL_SIZE = 10;
+    private static final int RANGE = 10_000;
+    private static final int NUMBER_ELEMENTS = 1_000_000;
+
+    public static void main(String[] args){
+
         // Creating list of numbers
         List<Integer> integers = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < NUMBER_ELEMENTS; i++) {
             integers.add(random.nextInt((100 - 1) + 1) + 1);
         }
 
         // Creating and executing our threads
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
         int sum = 0;
         int fromIndex = 0;
-        int toIndex = 100000;
-        for (int i = 0; i < 10; i++) {
+        int toIndex = RANGE;
+        for (int i = 0; i < POOL_SIZE; i++) {
             Future<Integer> subSum = executorService.submit(
                     new ThreadGetSumUsingExecutor(integers.subList(fromIndex, toIndex)));
-            fromIndex += 100000;
-            toIndex += 100000;
-            sum += subSum.get();
+            fromIndex += RANGE;
+            toIndex += RANGE;
+            try {
+                sum += subSum.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println(sum);
         executorService.shutdown();
--- src/main/java/ForkJoinMainTest.java
+++ src/main/java/ForkJoinMainTest.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 
 public class ForkJoinMainTest {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    private static final int POOL_SIZE = 10;
+    private static final int RANGE = 10_000;
+    private static final int NUMBER_ELEMENTS = 1_000_000;
+
+    public static void main(String[] args) {
         // Creating list of numbers
         List<Integer> integers = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < NUMBER_ELEMENTS; i++) {
             integers.add(random.nextInt((100 - 1) + 1) + 1);
         }
 
         // Using ForkJoinPool
         int sum = 0;
         int fromIndex = 0;
-        int toIndex = 100000;
-        for (int i = 0; i < 10; i++) {
+        int toIndex = RANGE;
+        for (int i = 0; i < POOL_SIZE; i++) {
             Integer subSum = new ForkJoinPool().invoke(
                     new SumCounterUsingForkJoin(integers.subList(fromIndex, toIndex)));
-            fromIndex += 100000;
-            toIndex += 100000;
+            fromIndex += RANGE;
+            toIndex += RANGE;
             sum += subSum;
         }
         System.out.println(sum);
